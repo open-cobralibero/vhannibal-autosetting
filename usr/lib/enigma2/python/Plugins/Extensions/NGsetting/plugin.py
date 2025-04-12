6df56cf56e29a5c212d9ad0c0f2d5aee4ba51a6a
Open-cobralibero <121136157+open-cobralibero@users.noreply.github.com>
2025-04-12 18:24:27 +0200

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/NGsetting/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/NGsetting/plugin.py
@@ -307,8 +307,8 @@ class MenuiSettingE2(Screen, ConfigListScreen):
 			self.ServerOn = False
 			self.jB.append(self.hauptListEntryMenuB(_('NGsetting Version ') + Version, '', '', '', ''))
 			self.jB.append(self.hauptListEntryMenuB(_('Coder: m43c0 & ftp21'), '', '', '', ''))
-			self.jB.append(self.hauptListEntryMenuB(_('Edit by: @Lululla 20231201'), '', '', '', ''))
-			self.jB.append(self.hauptListEntryMenuB(_('Skinner: mmark'), '', '', '', ''))
+			self.jB.append(self.hauptListEntryMenuB(_('Edit by: @Cobraliberosat 2025'), '', '', '', ''))
+			self.jB.append(self.hauptListEntryMenuB(_('Skinner: CBL'), '', '', '', ''))
 			self.jB.append(self.hauptListEntryMenuB(_('Vhannibal Official Plugin'), '', '', '', ''))
 			self.jB.append(self.hauptListEntryMenuB(_('www.vhannibal.net'), '', '', '', ''))
 			self["B"].setList(self.jB)
